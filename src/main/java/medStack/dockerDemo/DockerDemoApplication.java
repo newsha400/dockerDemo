04bfa284e442448d4f0f783275e72e7640eeceac
Newsha Tavakoli <newsha_tavakoli@homedepot.com>
2019-03-03 16:33:31 -0500

    u

--- src/main/java/medStack/dockerDemo/DockerDemoApplication.java
+++ src/main/java/medStack/dockerDemo/DockerDemoApplication.java
@@ -1,11 +1,14 @@
 package medStack.dockerDemo;
 
+import java.util.List;
+
 import org.glassfish.jersey.internal.inject.InjectionManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.ListContainersCmd;
+import com.github.dockerjava.api.model.Container;
 import com.github.dockerjava.api.model.Info;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
@@ -17,29 +20,21 @@ public class DockerDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoApplication.class, args);
 		
-		System.out.println("Newsha");
-		
-		String dockerVersion = DefaultDockerClientConfig.API_VERSION;
-//		System.out.println(dockerVersion);
-		
-//		DockerClient docker = DockerClientBuilder.getInstance().build();
-//		ListContainersCmd containerList = docker.listContainersCmd();
-//		System.out.println(containerList);
-		
-//		DockerClient dockerClient = DefaultDockerClientConfig.DOCKER_HOST;
-//		String dockerClient = DefaultDockerClientConfig.DOCKER_HOST;
-		
-		
+		System.out.println("start to build config");
 		DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
-				  .withDockerHost("tcp://localhost:2375")
+				  .withDockerHost("tcp://34.73.14.11:2375")
+				  .withDockerTlsVerify(false)
 				  .build();
-
+		System.out.println("start to build docker client");
 		DockerClient dockerClient = DockerClientBuilder.getInstance(config)
 				  .build();
+		System.out.println("Start to make info");
 		Info info = dockerClient.infoCmd().exec();
+		System.out.println("start to print info");
 		System.out.print(info);
-//		dockerClient.listContainersCmd();
-//		System.out.println(dockerClient.toString());
+//		List<Container> containersList = dockerClient.listContainersCmd().exec();
+//		for (Container c: containersList) {
+//			System.out.println(c.toString());
+//		}
 	}
-
 }
