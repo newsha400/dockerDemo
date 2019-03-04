a9a2e98d328d1a0c77b8a0edd45e67b21474def2
Newsha Tavakoli <newsha_tavakoli@homedepot.com>
2019-03-03 23:46:38 -0500

    u

--- src/main/java/medStack/dockerDemo/ContainerDao.java
+++ src/main/java/medStack/dockerDemo/ContainerDao.java
@@ -6,19 +6,17 @@ import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.Container;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
-import com.github.dockerjava.core.DockerClientConfig;
 
 @Component
 public class ContainerDao {
 
 	public List<Container> getContainers() {
-		DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
-				  .withDockerHost("tcp://34.73.14.11:2375")
-				  .withDockerTlsVerify(false)
-				  .build();
+
+		DefaultDockerClientConfig config= DefaultDockerClientConfig.createDefaultConfigBuilder().build();
 		DockerClient dockerClient = DockerClientBuilder.getInstance(config)
-				  .build();
-		List<Container> containersList = dockerClient.listContainersCmd().exec();
+		  .build();		
+		
+		 List<Container> containersList = dockerClient.listContainersCmd().exec();
 		return containersList;
 	}
 }
